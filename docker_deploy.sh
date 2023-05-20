4d2f12a581684f4140c0ff6b1279b532adca054f
aanorlondo <khaled.khebbeb@gmail.com>
2023-05-20 04:53:21 +0200

    fix: LAN resolving

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -4,8 +4,15 @@
 docker rm -f LAN-PROXY-LOCAL
 docker rmi negan/lan-proxy:local
 
+
+# dynamically configure the nginx proxy to be in the same network than the host
+host_ip=$(ipconfig getifaddr en0)
+
+
 # build the proxy
 docker build -t negan/lan-proxy:local .
+
+# push the image to remote
 docker push negan/lan-proxy:local
 
 
@@ -13,5 +20,6 @@ docker push negan/lan-proxy:local
 docker run \
     -d \
     -p 80:80 \
-    --name LAN-PROXY-LOCAL\
+    --name LAN-PROXY-LOCAL \
+    --add-host="host:${host_ip}" \
     negan/lan-proxy:local
