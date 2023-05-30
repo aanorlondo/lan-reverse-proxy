a089483170b8f7910cba8de235d73c92b4cedd95
aanorlondo <khaled.khebbeb@gmail.com>
2023-05-30 17:35:22 +0200

    feat: fields as <p>

--- homepage/src/components/AppFrame.js
+++ homepage/src/components/AppFrame.js
@@ -1,6 +1,11 @@
+// http://macbookpro-de-khaled.lan
+
 import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../App';
 import './AppFrame.css';
+import python_logo from "../assets/media/appframe/python_logo.png";
+import flask_logo from "../assets/media/appframe/flask_logo.png";
+import psql_logo from "../assets/media/appframe/psql_logo.png";
 
 function AppFrame() {
     const { selected_app_id } = useContext(AppContext);
@@ -20,7 +25,7 @@ function AppFrame() {
 
     const fetchAppDetails = async () => {
         try {
-            const response = await fetch(`/appdetails-api/?app_id=${selected_app_id}`);
+            const response = await fetch(`http://macbookpro-de-khaled.lan/appdetails-api/?app_id=${selected_app_id}`);
             const data = await response.json();
             if (response.ok) {
                 setAppDetails(data.data || {}); // Use an empty object if data is null or undefined
@@ -91,119 +96,137 @@ function AppFrame() {
 
     return (
         <div id="app-details">
-            <h2>{isEditing ? 'Edit App Details' : appDetails.name}</h2>
-            <p>
-                Name:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="name"
-                        value={editedAppDetails.name}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.name
-                )}
-            </p>
-            <p>
-                Description:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="description"
-                        value={editedAppDetails.description}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.description
-                )}
-            </p>
-            <p>
-                URL:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="url"
-                        value={editedAppDetails.url}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.url
-                )}
-            </p>
-            <p>
-                Frontend:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="frontend_technology"
-                        value={editedAppDetails.frontend_technology}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.frontend_technology
-                )}
-            </p>
-            <p>
-                Backend:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="backend_technology"
-                        value={editedAppDetails.backend_technology}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.backend_technology
-                )}
-            </p>
-            <p>
-                Database:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="database_technology"
-                        value={editedAppDetails.database_technology}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.database_technology
-                )}
-            </p>
-            <p>
-                Author:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="author"
-                        value={editedAppDetails.author}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.author
-                )}
-            </p>
-            <p>
-                Github:
-                {isEditing ? (
-                    <input
-                        type="text"
-                        name="github_url"
-                        value={editedAppDetails.github_url}
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    appDetails.github_url
-                )}
-            </p>
-            {isEditing ? (
-                <>
-                    <button onClick={handleSaveClick}>Save</button>
-                    <button onClick={handleCancelClick}>Cancel</button>
-                </>
-            ) : (
-                <button onClick={handleEditClick}>Edit</button>
-            )}
+            <div className="app-fields">
+                <h2 id="app-name-title">{isEditing ? 'Edit App Details' : appDetails.name || 'Not named yet'}</h2>
+                <p className='appdetails-entry'>
+                    Name:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="name"
+                            value={editedAppDetails.name}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.name
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Description:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="description"
+                            value={editedAppDetails.description}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.description
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    URL:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="url"
+                            value={editedAppDetails.url}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.url
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Frontend:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="frontend_technology"
+                            value={editedAppDetails.frontend_technology}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.frontend_technology
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Backend:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="backend_technology"
+                            value={editedAppDetails.backend_technology}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.backend_technology
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Database:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="database_technology"
+                            value={editedAppDetails.database_technology}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.database_technology
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Author:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="author"
+                            value={editedAppDetails.author}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.author
+                    )}
+                </p>
+                <p className='appdetails-entry'>
+                    Github:
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            name="github_url"
+                            value={editedAppDetails.github_url}
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        appDetails.github_url
+                    )}
+                </p>
+                <div className='buttons-div'>
+                    {isEditing ? (
+                        <>
+                            <button onClick={handleSaveClick}>Save</button>
+                            <button onClick={handleCancelClick}>Cancel</button>
+                        </>
+                    ) : (
+                        <button onClick={handleEditClick}>Edit</button>
+                    )}
+                </div>
+                <div id='tech-list'>
+                    <p>
+                        Powered by
+                    </p>
+                    <p>
+                        <img id="python-logo" src={python_logo}></img>
+                    </p>
+                    <p>
+                        <img id="flask-logo" src={flask_logo}></img>
+                    </p>
+                    <p>
+                        <img id="psql-logo" src={psql_logo}></img>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
