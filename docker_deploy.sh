5f7a47685df21dbb6ca85153699fd0421af2d81c
aanorlondo <khaled.khebbeb@gmail.com>
2023-06-06 19:32:00 +0200

    feat: added authentication

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# prepare env vars
+sh ./prepare_env.sh
+
 # clean first
 docker rm -f LAN-PROXY-LOCAL
 docker rmi negan/lan-proxy:local
 
-
 # dynamically configure the nginx proxy to be in the same network than the host
 host_ip=$(ipconfig getifaddr en0)
-
 
 # build the proxy
 docker build -t negan/lan-proxy:local .
@@ -15,12 +16,12 @@ docker build -t negan/lan-proxy:local .
 # push the image to remote
 docker push negan/lan-proxy:local
 
-
 # run the proxy
 docker run \
     -d \
     -p 80:80 \
     -p 443:443 \
-    --name LAN-PROXY-LOCAL \
+    -e AUTH_SERVER=${AUTH_SERVER} \
     --add-host="host:${host_ip}" \
+    --name LAN-PROXY-LOCAL \
     negan/lan-proxy:local
